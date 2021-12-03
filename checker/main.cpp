7efb35a3a91fa91ef7420162c30487b548f0bd5f
Ryan Trinidad <siro9mm@gmail.com>
2021-12-03 19:39:48 +0100

    create build deck function using vectors

--- checker/main.cpp
+++ checker/main.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 using namespace std;
 
-/*
- * Create list of numbers from 2 to 15
- * Create array of suits H,S,C,D
- * Create empty deck array
- * Build deck of cards per suit and store into deck array
- */
-int build_deck()
+vector <string> build_deck()
 {
-    //int num_list [] {2,3,4,5,6,7,8,9,10,11,12,13,14};
     vector <int> card_numbers {2,3,4,5,6,7,8,9,10,11,12,13,14};
     vector <char> suits {'H','S','C','D'};
+    vector <string> deck;
 
-    for (int i = 0; i < card_numbers.size(); i++)
+    for ( int num: card_numbers )
     {
-        cout << card_numbers[i] << " ";
+        for ( char suit: suits )
+        {
+            deck.push_back( suit + to_string( num ) );
+        }
     }
 
-    cout << endl;
-
-    for (int i = 0; i < suits.size(); i++)
-    {
-        cout << suits[i] << " ";
-    }
-
-    cout << endl;
-
-    // TODO: Build deck of 52 cards
-
-    return 0;
+    return deck;
 }
 
 
@@ -41,11 +29,19 @@ int combinations()
 }
 
 
-int main(int argc, char **argv)
+int main ( int argc, char **argv )
 {
 	cout << "poker odds tool\n" << endl;
 
-	return build_deck();
+    vector <string> cards;
+    cards = build_deck();
+
+    for ( string card: cards )
+    {
+        cout << card << endl;
+    }
+
+    return 0;
 }
 
 
